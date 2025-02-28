d7f0daef3bd61724e0565bccda5f8b2b6f6ba0ea
RendevMq <rensystem73@gmail.com>
2025-02-28 12:41:03 -0500

    cartActivity UI & itemviewholder

---
+++ app/src/main/java/com/rensystem/p01_petshop/Activity/CartActivity.kt
@@ -0,0 +1,16 @@
+package com.rensystem.p01_petshop.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rensystem.p01_petshop.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+
+    }
+}
